76ccaa0c177f5f06d9ebc664ef350366c4cdb254
dzmitry-belush <dzmitry.belush@gmail.com>
2023-01-12 19:32:35 +0100

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function reverse (n) {
-  
+// function reverse (n) { // selftest
+    let reversed = 0;
+    let reversedStr = '';
+
+    let number = Math.abs(n);
+    let numberStr = number.toString();
+
+    for (let i = 0; i < numberStr.length; i++) {
+        reversedStr = `${numberStr[i]}${reversedStr}`
+    }
+
+    reversed = Number(reversedStr);
+
+    console.log(typeof reversed, reversed);
+    return reversed;
 }
+
+// reverse(-54321); // selftest
